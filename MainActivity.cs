2e306bee9aa9097b8f0947f649163f74c7e1f63a
draper21 <joeydraper@aol.com>
2020-02-11 21:36:37 -0500

    update

--- MainActivity.cs
+++ MainActivity.cs
@@ -12,15 +12,60 @@ using System.Collections.Generic;
 namespace Draper_CIT414_Assignment2_Intents
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    public class MainActivity : Activity
     {
+        Button callButton;
+        Button emailButton;
+        Button mapsButton;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            SetContentView(Resource.Layout.activity_main);
+            SetContentView(Resource.Layout.Main);
 
+            callButton = FindViewById<Button>(Resource.Id.callButton);
+            callButton.Click += CallButton_Click;
+
+            emailButton = FindViewById<Button>(Resource.Id.emailButton);
+            emailButton.Click += EmailButton_Click;
+
+            mapsButton = FindViewById<Button>(Resource.Id.mapsButton);
+            mapsButton.Click += MapsButton_Click;
+          }
+
+        //send email
+        private void EmailButton_Click(object sender, EventArgs e)
+        {
+            var intent = new Intent();
+            intent.PutExtra(Android.Content.Intent.ExtraEmail, new String[] {
+                "maurod@marshall.edu"
+            });
+            intent.PutExtra(Android.Content.Intent.ExtraSubject, "Testing an email from Xamarin");
+            intent.PutExtra(Android.Content.Intent.ExtraText, "Testing Body of the email message.");
+            intent.SetType("message/rfc822");
+            StartActivity(intent);
+        }
+
+        //open second activity which then leads to making the phone call
+        private void CallButton_Click(object sender, EventArgs e)
+        {
+            //Create an intent and start an activity
+            var intent = new Intent(this, typeof(PhoneActivity));
+            StartActivity(intent);
+        }
+
+        //open maps to morrow library
+        private void MapsButton_Click(object sender, EventArgs e)
+        {
+            //Create an intent and start an activity
+            var intent = new Intent();
+            intent.SetAction(Intent.ActionView);
+            intent.SetData(Android.Net.Uri.Parse("geo:0,0?q=Morrow+Library+Huntington+WV"));
+            if (intent.ResolveActivity(PackageManager)!= null)
+                {
+                    StartActivity(intent);
+                }
         }
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
---
+++ PhoneActivity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Draper_CIT414_Assignment2_Intents
+{
+    [Activity(Label = "PhoneActivity")]
+    public class PhoneActivity : Activity
+    {
+        Button makeCallButton;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Phone);
+            makeCallButton = FindViewById<Button>(Resource.Id.makeCallButton);
+            makeCallButton.Click += MakeCallButton_Click;
+        }
+        //make phone call 
+        private void MakeCallButton_Click(object sender, EventArgs e)
+        {
+            var intent = new Intent(Intent.ActionDial, Android.Net.Uri.Parse("tel:8323739909"));
+            StartActivity(intent);
+        }
+
+    }
+}
